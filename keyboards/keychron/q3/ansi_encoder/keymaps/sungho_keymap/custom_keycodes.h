193bfab31ccd629ec4b17a3e602594b3b751bbe6
sungho55 <sungho55.kim@gmail.com>
2025-04-16 15:05:15 +0900

    add screen capture

--- keyboards/keychron/q3/ansi_encoder/keymaps/sungho_keymap/custom_keycodes.h
+++ keyboards/keychron/q3/ansi_encoder/keymaps/sungho_keymap/custom_keycodes.h
@@ -3,3 +3,4 @@
 #define KC_GPT LGUI(LCTL(KC_A))     // ChatGPT
 #define KC_CRSR LGUI(LCTL(KC_W))    // Cursor
 #define KC_CHRM LGUI(LCTL(KC_B))    // Chrome
+#define KC_CPTR LGUI(LSFT(KC_5))    // Screen Capture
--- keyboards/keychron/q3/ansi_encoder/keymaps/sungho_keymap/keymap.c
+++ keyboards/keychron/q3/ansi_encoder/keymaps/sungho_keymap/keymap.c
@@ -22,7 +22,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_f13_ansi(
-        KC_ESC,   KC_CHRM,  KC_F2,    KC_MCTL,  KC_LPAD,  KC_CRSR,  KC_GPT,   KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_F11,   KC_F12,     KC_MUTE,  KC_NO,    KC_NO,    TD(TD_SLP),
+        KC_ESC,   KC_CHRM,  KC_F2,    KC_MCTL,  KC_LPAD,  KC_CRSR,  KC_GPT,   KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_F11,   KC_F12,     KC_MUTE,  KC_CPTR,  KC_NO,    TD(TD_SLP),
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,

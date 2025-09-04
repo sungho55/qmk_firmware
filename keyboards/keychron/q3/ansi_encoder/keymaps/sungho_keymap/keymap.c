b56843c1990b881c1860dbf13df42938136553d2
Sungho Kim <sungho55.kim@gmail.com>
2025-09-04 12:43:13 +0900

    feat: remove claude key

--- keyboards/keychron/q3/ansi_encoder/keymaps/sungho_keymap/keymap.c
+++ keyboards/keychron/q3/ansi_encoder/keymaps/sungho_keymap/keymap.c
@@ -19,7 +19,6 @@
 #include "custom_keycodes.h"
 #include "combos.h"
 #include "macos_keys.c"
-#include "claude_keys.c"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_f13_ansi(
@@ -72,10 +71,6 @@ void keyboard_post_init_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_macos_keys(keycode, record)) {
         return false;
-    }
-    if (!process_claude_keys(keycode, record)) {
-        return false;
-    }
-    
+    }    
     return true;
 }
